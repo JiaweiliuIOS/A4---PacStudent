11fc8072a21b32ecc546661259010a34a038fd18
Jiawei Liu <12638430@student.uts.edu.au>
2021-11-01 20:30:46 +1100

    Assessment 4 pass

--- Assets/Script/Movement.cs
+++ Assets/Script/Movement.cs
@@ -22,16 +22,16 @@ public class Movement : MonoBehaviour
 
     void input(){
         
-        if (Input.GetKeyDown (KeyCode.RightArrow)){
+        if (Input.GetKeyDown (KeyCode.D)){
             direction = Vector2.right;
 
-        }else if (Input.GetKeyDown (KeyCode.LeftArrow)){
+        }else if (Input.GetKeyDown (KeyCode.A)){
             direction = Vector2.left;
 
-        }else if (Input.GetKeyDown (KeyCode.UpArrow)){
+        }else if (Input.GetKeyDown (KeyCode.W)){
             direction = Vector2.up;
 
-        }else if (Input.GetKeyDown (KeyCode.DownArrow)){
+        }else if (Input.GetKeyDown (KeyCode.S)){
             direction = Vector2.down;
         }
 
