84334ed85c4581c1785f93476d78ad77b75a63a0
JiaweiliuIOS <83327258+JiaweiliuIOS@users.noreply.github.com>
2021-11-04 16:49:51 +1100

    70 percent

---
+++ Assets/Script/AudioManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource bgm;
+    public AudioSource newTrack;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bgm.Play();
+        newTrack.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D)){
+            bgm.Stop();
+            newTrack.Play();
+        }else if (Input.GetKeyDown(KeyCode.S)){
+            bgm.Stop();
+            newTrack.Play();
+        }
+    }
+
+    
+    
+}
